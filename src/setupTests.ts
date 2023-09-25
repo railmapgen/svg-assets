225ab0f3ea17131abcfcef23a3c64ef9782708a4
Chito Wong <chi.to.wong@outlook.com>
2023-09-25 20:25:44 +0800

    Initial commit

---
+++ src/setupTests.ts
@@ -0,0 +1,28 @@
+import { RootState } from './redux';
+import createMockStore from 'redux-mock-store';
+import { getDefaultMiddleware, ThunkDispatch } from '@reduxjs/toolkit';
+import infoJson from '../info.json';
+import { TextEncoder } from 'util';
+import { vi } from 'vitest';
+import { MockBroadcastChannel } from './mock-broadcast-channel';
+
+// FIXME: any -> AnyAction?
+type DispatchExts = ThunkDispatch<RootState, void, any>;
+export const createMockRootStore = createMockStore<RootState, DispatchExts>(getDefaultMiddleware());
+
+vi.stubGlobal('BroadcastChannel', MockBroadcastChannel);
+
+const originalFetch = global.fetch;
+global.fetch = (...args) => {
+    if (args[0].toString().includes('/info.json')) {
+        return Promise.resolve({
+            ok: true,
+            status: 200,
+            json: () => Promise.resolve(infoJson),
+        }) as any;
+    } else {
+        return originalFetch(...args);
+    }
+};
+
+global.TextEncoder = TextEncoder;
