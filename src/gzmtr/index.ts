80276eefa78ed19f31e79ab6cecb1a28e72dd0c6
Chito Wong <chi.to.wong@outlook.com>
2024-05-20 21:12:52 +0800

    #1 Export GZMTR facility icons

--- src/gzmtr/index.ts
+++ src/gzmtr/index.ts
@@ -1,4 +1,7 @@
+import AirportIcon from './icons/airport-icon';
+import IntercityIcon from './icons/intercity-icon';
+import RailwayIcon from './icons/railway-icon';
 import LineIcon from './line-icon/line-icon';
 import StationNumber from './station-icon/station-number';
 
-export { LineIcon, StationNumber };
+export { LineIcon, StationNumber, AirportIcon, IntercityIcon, RailwayIcon };
