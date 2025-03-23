a0af19f0c50a1a3e5a33e011ee8eb8d8d214137b
Chito Wong <chi.to.wong@outlook.com>
2025-03-23 14:26:03 +0800

    Upgrade storybook

--- build.config.ts
+++ build.config.ts
@@ -2,6 +2,9 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
     rollup: {
+        output: {
+            compact: true,
+        },
         esbuild: {
             jsx: 'automatic',
         },
