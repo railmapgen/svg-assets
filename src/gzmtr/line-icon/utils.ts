fbda97e5dea098fd5ccb0f3706940c5565e31e15
Chito Wong <chi.to.wong@outlook.com>
2026-04-15 21:36:23 +0800

    #8 GZMTR - Support spanning common part in a line box without an uncommon part

--- src/gzmtr/line-icon/utils.ts
+++ src/gzmtr/line-icon/utils.ts
@@ -2,7 +2,7 @@ export const DEFAULT_BOX_WIDTH = 45;
 export const MAX_TEXT_WIDTH = DEFAULT_BOX_WIDTH - 3;
 
 export const getCommonStarts = (zhName: string, enName: string) => {
-    const matches = [zhName, enName].map(text => text.match(/^(\w+).+$/));
+    const matches = [zhName, enName].map(text => text.match(/^(\w+).*$/));
     if (matches[0] && matches[1] && matches[0][1] === matches[1][1]) {
         return matches[0][1];
     }
